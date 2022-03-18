26db51f14e65d6a886a401e59610af93b2a3cd3c
mxt629 <xuantien.06021990@gmail.com>
2022-03-18 11:14:54 +0700

    Initial Commit

---
+++ SS8 Ex3/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SS8 Ex3
+//
+//  Created by Little Chick on 18/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
